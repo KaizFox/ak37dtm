50465d64ba11b54d4c8cd454a0e16873d4ea331c
KaizOffical <106258613+KaizFox@users.noreply.github.com>
2022-10-25 20:36:08 +0700

    upload system

--- upload.php
+++ upload.php
@@ -1,16 +1,6 @@
 <?php
-
-/* Get the name of the uploaded file */
 $filename = $_FILES['file']['name'];
-
-/* Choose where to save the uploaded file */
-$location = "upload/".$filename;
-
-/* Save the uploaded file to the local filesystem */
-if ( move_uploaded_file($_FILES['file']['tmp_name'], $location) ) {
-  echo 'Success';
-} else {
-  echo 'Failure';
-}
-
+$tmpname = $_FILES['file']['tmp_name'];
+$fileup_name = time().$filename;
+move_uploaded_file($tmpname, "upload/".$fileup_name);
 ?>
